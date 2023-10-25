d1d93f328c125a1d72f04b8abc035b33445d896f
ved-thakre <vedantthakre7@gmail.com>
2023-10-25 19:59:59 +0530

    homepage done

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -6,12 +6,14 @@ import {
   TabPanel,
   TabPanels,
   Tabs,
-  Text,
+  Image,
+  Center,
 } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
 import Login from "../components/Authentication/Login";
 import Signup from "../components/Authentication/Signup";
+import LogoDark from "../assets/Darklogo.png";
 
 function Homepage() {
   const navigate = useNavigate();
@@ -22,37 +24,36 @@ function Homepage() {
     if (user) navigate("/chats");
   }, [navigate]);
 
-  return(
-    <Container maxW="xl" centerContent>
-      <Box
-        display="flex"
-        justifyContent="center"
-        p={3}
-        bg="white"
-        w="100%"
-        m="40px 0 15px 0"
-        borderRadius="lg"
-        borderWidth="1px"
-      >
-        <Text fontSize="4xl" fontFamily="Work sans">
-ConnectMe        </Text>
-      </Box>
-      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
-        <Tabs isFitted variant="soft-rounded">
-          <TabList mb="1em">
-            <Tab>Login</Tab>
-            <Tab>Sign Up</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <Login />
-            </TabPanel>
-            <TabPanel>
-              <Signup />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Box>
+  return (
+    <Container maxW="xl">
+      <Center h="100vh">
+        <Box
+          bg="white"
+          w="100%"
+          pr={4}
+          pl={4}
+          pb={4}
+          pt={2}
+          borderRadius="lg"
+          borderWidth="1px"
+        >
+          <Image w="180px" mx="auto" src={LogoDark} alt="Logo" mb={5} />
+          <Tabs isFitted variant="soft-rounded">
+            <TabList mb="1em">
+              <Tab _selected={{ color: "black", bg: "#808080" }}>Login</Tab>
+              <Tab _selected={{ color: "black", bg: "#808080" }}>Sign Up</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <Login />
+              </TabPanel>
+              <TabPanel>
+                <Signup />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+      </Center>
     </Container>
   );
 }
