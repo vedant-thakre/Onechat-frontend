8ac088bad44f6e75e4d75fd512e83005a33e3ef1
ved-thakre <vedantthakre7@gmail.com>
2023-10-25 20:05:24 +0530

    background changed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import "./App.css";
 import Homepage from "./Pages/Homepage";
 import { Route, Routes } from "react-router-dom";
 import Chatpage from "./Pages/Chatpage";
+import { ChatState } from "./Context/ChatProvider";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
+    const { mode } = ChatState();
 
-      <Route path="/" Component={Homepage}  exact />
-      <Route path="/chats" Component={Chatpage} />
+    const appStyles = {
+      minHeight: "100vh",
+      display: "flex",
+      background: mode
+        ? "#000000"
+        : "radial-gradient(circle at 24.1% 68.8%, rgb(50, 50, 50) 0%, rgb(0, 0, 0) 99.4%)",
+    };
+
+  return (
+    <div className="App" style={appStyles}>
+      <Routes>
+        <Route path="/" Component={Homepage} exact />
+        <Route path="/chats" Component={Chatpage} />
       </Routes>
     </div>
   );
